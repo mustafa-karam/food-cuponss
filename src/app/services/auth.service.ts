6e55b1ab702921f6808ec2a7bb390cf08b0d281f
mustafa-karam <163323770+mustafa-karam@users.noreply.github.com>
2024-08-28 21:35:10 +0300

    veryveryLast

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,7 +19,7 @@ getEmployeeById(id:any):Observable<User[]>{
   return this.http.get<User[]>(`${this.baseUrl}/api/User/searchIds?searchword=${id}`);
 }
 generateQrCode(requests:QrCodeRequest[]):Observable<any>{
-  return this.http.post<any[]>(`${this.baseUrl}/api/UserQrCode/GenerateMultipleCodes/generate-multiple`,requests)
+  return this.http.post<any[]>(`${this.baseUrl}/api/UserQrCode/GenerateMultipleQrCodes/generate-multiple`,requests)
 }
 SaveScanResult(id:string):Observable<any>{ 
   return this.http.post<any>(`${this.baseUrl}/api/UserQrCode/SaveScanResult`,JSON.stringify(id),{
